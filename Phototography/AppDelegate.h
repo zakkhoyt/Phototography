e65179bca1f420a411569563b81d23a1797d450c
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-10-02 15:54:18 -0700

    Initial Commit

---
+++ Phototography/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Phototography
+//
+//  Created by Zakk Hoyt on 10/2/15.
+//  Copyright © 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Phototography/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Phototography
+//
+//  Created by Zakk Hoyt on 10/2/15.
+//  Copyright © 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
